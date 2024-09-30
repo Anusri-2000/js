231fcdef5af2e2503440628a81eb28356be1125d
Anusri-2000 <141505912+Anusri-2000@users.noreply.github.com>
2024-09-30 13:07:15 -0700

    Add files via upload

---
+++ getelementclassname.js
@@ -0,0 +1,5 @@
+
+
+const x = document.getElementsByClassName("intro");
+document.getElementById("demo").innerHTML = 
+'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
---
+++ selector.js
@@ -0,0 +1,2 @@
+const x=document.querySelectorAll("p.intro");
+document.getElementById("demo").innerHTML='The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
